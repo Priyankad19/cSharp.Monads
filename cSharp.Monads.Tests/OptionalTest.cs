ab1649824f0263c46de3a49806507753bce1eaca
Priyanka <Priyanka.dhoble@bitwiseglobal.com>
2016-05-04 18:40:21 +0530

    Implementation on Optional type in C#

---
+++ cSharp.Monads.Tests/OptionalTest.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using cSharp.Monads;
+
+namespace cSharp.Monads.Tests
+{
+    /// <summary>
+    /// Summary description for OptionalTest
+    /// </summary>
+    [TestClass]
+    public class OptionalTest
+    {
+        public OptionalTest()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void shouldReturnTrueForSomeValuePassed()
+        {
+            String s = "Priyanka";
+            Optional<String> optionalString = s;
+
+            Assert.AreEqual(true, optionalString.HasValue);
+        }
+
+        [TestMethod]
+        public void shouldReturnFalseForNullValuePassed()
+        {
+            String s = null;
+            Optional<String> optionalString = s;
+
+            Assert.AreEqual(false, optionalString.HasValue);
+        }
+
+       
+        [TestMethod]
+        public void shouldReturnDefaultValueForNullPassedOnString()
+        {
+            String input = null;
+            Optional<String> optionalString = input;
+            String defaultValue = "IamDefault";
+            Assert.AreEqual(defaultValue,optionalString.GetOrElse(defaultValue));
+        }
+
+        [TestMethod]
+        public void shouldReturnDefaultValueForNullPassedOnClass()
+        {
+            TestInput input = null;
+            Optional<TestInput> optionalString = input;
+            TestInput defaultValue = new TestInput(0,"IamDefault");
+            Assert.AreEqual(defaultValue, optionalString.GetOrElse(defaultValue));
+        }
+
+        [TestMethod]
+        public void shouldReturnActualValueForValuePassedOnClass()
+        {
+            TestInput input = new TestInput(1,"HiIamThere");
+            Optional<TestInput> optionalString = input;
+            TestInput defaultValue = new TestInput(0, "IamDefault");
+            Assert.AreEqual(input, optionalString.GetOrElse(defaultValue));
+        }
+
+        [TestMethod]
+        public void shouldCallFunctionIfValueIsPresent()
+        {
+            int input = 2;
+            Optional<int> optionalValue = input;
+            optionalValue.IfPresent(CallMe<int>);
+        }
+
+        private void CallMe<T>(T value)
+        {
+            Assert.AreEqual(2, value);
+        }
+
+        public class TestInput
+        {
+            string name;
+            int id;
+            public TestInput(int id, string name)
+            {
+                this.name = name;
+                this.id = id;
+            }
+
+            public override bool Equals(object obj)
+            {
+                if(obj == null)
+                    return false;
+                TestInput secondObject = (TestInput)obj;
+                if (this.id == secondObject.id && this.name == secondObject.name)
+                    return true;
+                return false;
+            }
+
+        }
+    }
+}
---
+++ cSharp.Monads/Optional.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace cSharp.Monads
+{
+
+    public abstract class Optional<T>
+    {
+        // Could contain the value if Some, but not if None
+        protected abstract T Value { get; }
+
+        /// <summary>
+        /// Returns true in case of Some otherwise false
+        /// </summary>
+        public abstract bool HasValue { get; }
+
+        /// <summary>
+        /// Get value. Not accessible outside this class forcing developers to use GetOrElse, HasValues or IfPresent. 
+        /// This will prevent side effect of using just Get
+        /// </summary>
+        /// <returns></returns>
+        private T Get()
+        {
+            return Value;
+        }
+
+        /// <summary>
+        /// Get the value if present otherwise returns default value passed
+        /// </summary>
+        /// <param name="defaultValue">Default value</param>
+        /// <returns>Returns value if present otherwise default value</returns>
+        public T GetOrElse(T defaultValue)
+        {
+            if (HasValue)
+                return Get();
+            else
+                return defaultValue;
+        }
+
+        /// <summary>
+        /// Calls a function if the value is present otherwise does nothing
+        /// </summary>
+        /// <param name="consumerFunction">Function with one parameter of type T and no return type</param>
+        public void IfPresent(Action<T> consumerFunction)
+        {
+            if (HasValue)
+                consumerFunction(Value);
+        }
+
+        /// <summary>
+        /// Implicit conversion of any type into optional type
+        /// </summary>
+        /// <param name="value">Value</param>
+        /// <returns></returns>
+        public static implicit operator Optional<T>(T value)
+        {
+            if (value == null)
+                return new None<T>();
+            else
+                return new Some<T>(value);
+        }
+
+        /// <summary>
+        /// Explicitly convert Optional type to any given type
+        /// </summary>
+        /// <param name="value">Value to be converted</param>
+        /// <returns></returns>
+        public static explicit operator T(Optional<T> value)
+        {
+            return value.Value;
+        }
+    }
+
+    public sealed class Some<T> : Optional<T>
+    {
+        private T value;
+        public Some(T value)
+        {
+            // Setting Some to null, nullifies the purpose of Some/None
+            if (value == null)
+            {
+                throw new System.ArgumentNullException("value", "Some value was null, use None instead");
+            }
+
+            this.value = value;
+        }
+
+        protected override T Value { get { return value; } }
+
+        public override bool HasValue { get { return true; } }
+    }
+
+    public sealed class None<T> : Optional<T>
+    {
+        protected override T Value { get { throw new System.NotSupportedException("There is no value"); } }
+
+        public override bool HasValue { get { return false; } }
+    }
+}
